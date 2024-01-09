4d8cb62a1070825650eae10072cca1aff4d17079
Mithun.U <116871732+Mithun-750@users.noreply.github.com>
2024-01-10 00:45:25 +0530

    add Background(1) and Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,35 @@
+import { useEffect } from 'react';
+import Navbar from '../Components/Navbar/Navbar'
 import './App.css'
 
 function App() {
+  useEffect(() => {
+    window.addEventListener("scroll", function () {
+      if (this.window.innerWidth < 950) {
+        const scrollProgress = window.scrollY / (document.body.scrollHeight - window.innerHeight);
+
+        const topColor = `rgb(0, 30, 55) ${70 + scrollProgress * 30}%`;
+        const bottomColor = `rgb(3, 54, 86) ${70 - scrollProgress * 30}%`;
+
+        document.getElementById("root").style.setProperty("--gradient-top", topColor);
+        document.getElementById("root").style.setProperty("--gradient-bottom", bottomColor);
+
+      } else {
+
+        const scrollProgress = window.scrollY / (document.body.scrollHeight - window.innerHeight);
+
+        const topColor = `rgb(0, 30, 55) ${50 + scrollProgress * 50}%`;
+        const bottomColor = `rgb(3, 54, 86) ${50 - scrollProgress * 50}%`;
+
+        document.getElementById("root").style.setProperty("--gradient-top", topColor);
+        document.getElementById("root").style.setProperty("--gradient-bottom", bottomColor);
+      }
+    });
+  }, []);
 
   return (
     <>
+      <Navbar />
     </>
   )
 }
