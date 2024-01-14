cb1e91a0acf57095d8755fca640705c5133ce061
Mithun.U <116871732+Mithun-750@users.noreply.github.com>
2024-01-15 01:22:16 +0530

    test

--- Components/Warrior/Warrior.jsx
+++ Components/Warrior/Warrior.jsx
@@ -8,7 +8,7 @@ export const Warrior = ({ children }) => {
     return (
         <ScrollControls pages={(window.innerWidth < 500) ? 3.9 : 3.5}  >
             <Environment preset={"warehouse"} />
-            <Scroll html style={{ overflowY: "auto", height: "350vh" }} >
+            <Scroll html style={{ height: "350vh" }} >
                 {children}
             </Scroll>
             <Warrior_toy />
