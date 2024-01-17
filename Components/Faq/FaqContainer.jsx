738c388ff5378d3433cb3fc2cffdd40afa92bec3
aahnik <daw@aahnik.dev>
2024-01-17 10:25:29 +0530

    try making faq section look good on small screen sizes

--- Components/Faq/FaqContainer.jsx
+++ Components/Faq/FaqContainer.jsx
@@ -3,8 +3,8 @@ import "./Faq.css";
 export const FaqContainer = ({ children }) => {
   return (
     <div className="my-16">
-      <div className="flex justify-center">
-        <div className="space-y-4 max-w-4xl rounded-lg py-4 w-full FaqContainer shadow-2xl">
+      <div className="flex justify-center py-4">
+        <div className="space-y-4 md:max-w-4xl rounded-lg py-4 w-full FaqContainer shadow-2xl max-w-screen-sm mx-4 mr-4">
           {children}
         </div>
       </div>
